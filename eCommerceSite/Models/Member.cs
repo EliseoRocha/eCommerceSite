6a8c15623fec3b68ff44457ef0878ae956be6191
Eliseo Rocha <rochaeliseo91@gmail.com>
2019-01-22 20:16:31 -0800

    Creating Models and Views

---
+++ eCommerceSite/Models/Member.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    public class Member
+    {
+        //Data Annotations - Validation and DB markup
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        //int is automatically required because
+        //it is a value type
+        [Key] //Mark this field as PK (Primary Key)
+        public int MemberID { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)] //Enum/Enumeration
+        public string Password { get; set; }
+
+
+    }
+}
