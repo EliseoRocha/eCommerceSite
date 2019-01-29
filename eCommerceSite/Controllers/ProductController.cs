62c9c2c2a3625e21fa989e351f3d196286ed0c29
Eliseo Rocha <rochaeliseo91@gmail.com>
2019-01-29 10:54:10 -0800

    Added CRUD Functionality

---
+++ eCommerceSite/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceSite.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Add to DataBase
+                return View();
+            }
+
+            return View(p);
+        }
+    }
+}
